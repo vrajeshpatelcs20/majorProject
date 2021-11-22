7af704e1d9d8f65eeaaee6e37270878588ba8ef6
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-11-22 12:01:01 -0600

    added intial stuff

--- sketch.js
+++ sketch.js
@@ -1,15 +1,25 @@
-// First Demo
-// Your Name
-// Date
+// Major Project/ Battleship
+// Vrajesh Patel
+// 2021/2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let loadingScreen;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+}
+function stateOfGame(){
+  if (stateOfGame === loadingScreen) {
+    starterScreen();
+  }
+}
+
+function starterScreen(){
+  
 }
 
 function draw() {
   background(0);
+  stateOfGame;
 }
