8549964662a913c040aaa46f0e87f295144a8d12
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-12-02 12:10:52 -0600

    added more functions

--- sketch.js
+++ sketch.js
@@ -5,16 +5,26 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let stateOfGame = "loadingScreen";
+let stateOfGame;
 let theColor = 0;
 let fillColor = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  stateOfGame = "loadingScreen";
+
 }
 function stateChecker() {
   if (stateOfGame === "loadingScreen") {
     starterScreen();
+  }
+  if (stateOfGame === "instructionsOfBattleship") {
+    pregameBattleship();
+  }
+  if (stateOfGame === "battleshipGame"){
+
+    background(0);
+    battleshipGame();  
   }
 }
 
@@ -30,24 +40,41 @@ function starterScreen() {
   text("Battleship", width / 4 * 3, height / 2 + 10);
   text("Snake", width / 4 * 2, height / 2 + 10);
   text("Undecided", width / 4, height / 2 + 10);
-  textSize(40);
-  textAlign(CENTER); 
+  textSize(width / 40);
+  textAlign(CENTER);
   // eslint-disable-next-line no-extra-parens
-  if((mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 + 100 && mouseX > width / 4 - 100) || (mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 * 2 + 100 && mouseX > width / 4 * 2 - 100) || (mouseY < height / 2 + 50 && mouseY > height / 2 - 50 &&mouseX < width / 4 * 3 + 100 && mouseX > width / 4 * 3 - 100)){
+  if ((mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 + 100 && mouseX > width / 4 - 100) || (mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 * 2 + 100 && mouseX > width / 4 * 2 - 100) || (mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 * 3 + 100 && mouseX > width / 4 * 3 - 100)) {
     theColor = "blue";
     fillColor = 0;
   }
-  else{
+  else {
     theColor = 0;
     fillColor = "blue";
   }
 }
 
-function mousePressed() {
-  if(stateOfGame === "loadingScreen"){
-    if(mouseY < height / 2 + 50 && mouseY > height / 2 - 50 &&mouseX < width / 4 * 3 + 100 && mouseX > width / 4 * 3 - 100){
-      stateOfGame === "battleship";
+function pregameBattleship() {
+  stroke(0);
+  background(255);
+  text("Press Space to Countine", width / 2, 100);
+}
+battleshipGame(){
+  
+}
 
+function mousePressed() {
+  if (stateOfGame === "loadingScreen") {
+    if (mouseY < height / 2 + 50 && mouseY > height / 2 - 50 && mouseX < width / 4 * 3 + 100 && mouseX > width / 4 * 3 - 100) {
+      stateOfGame = "instructionsOfBattleship";
+
+    }
+  }
+}
+
+function keyPressed() {
+  if (key === " ") {
+    if (stateOfGame === "instructionsOfBattleship") {
+      stateOfGame = "battleshipGame";
     }
   }
 }
